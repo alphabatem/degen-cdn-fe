f4006774f150495111446b46a446d241ba3a0ec3
Cloakd <thecloakdone@gmail.com>
2023-02-05 18:14:29 +0000

    Tidy up search

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -2,29 +2,32 @@
 	<div class="container">
 		<h1>{{ $route.params.id }}</h1>
 
-		<div class="row">
-			<div class="img-container">
-				<img alt="" :src="previewLink" class="img-fluid">
-			</div>
-			<div class=" mt-2">
-				<button @click="toggleOriginal" class="btn btn-outline-light">Original</button>
-			</div>
-		</div>
-
 		<div class="row mt-5">
-			<div class="col-8 offset-2 text-start">
+			<div class="col text-start">
+				<h3>Details</h3>
 
-				<p class="mb-0">Original Image Link: </p>
+				<p class="mb-0 mt-3">Token Mint:</p>
+				<code>{{ media.mint }}</code>
+
+				<p class="mb-0 mt-3">Direct Image Link: </p>
+				<code>{{ directLink + '/image' + (media.imageType ? `.${media.imageType}` : '') || 'N/A' }}</code>
+
+				<p class="mb-0 mt-3">Direct Media Link: </p>
+				<code class="mt-5">{{ mediaUri ? directLink + '/media' + (media.mediaType ? `.${media.mediaType}` : '') : 'N/A' }}</code>
+
+				<p class="mb-0 mt-3">Original Image Link: </p>
 				<code>{{ imageFileUri || 'N/A' }}</code>
 
 				<p class="mb-0 mt-3">Original Media Link: </p>
 				<code class="mt-5">{{ mediaUri || 'N/A' }}</code>
-
-				<p class="mb-0 mt-3">Direct Image Link: </p>
-				<code>{{ directLink + '/image' || 'N/A' }}</code>
-
-				<p class="mb-0 mt-3">Direct Media Link: </p>
-				<code class="mt-5">{{ mediaUri ? directLink + '/media' : 'N/A' }}</code>
+			</div>
+			<div class="col">
+				<div class="img-container">
+					<img id="preview-img" alt="" :src="previewLink" class="img-fluid loading" @load="toggleImageLoading">
+				</div>
+				<div class=" mt-2" v-if="media.imageType !== 'gif'">
+					<button @click="toggleOriginal" class="btn btn-outline-light">Original</button>
+				</div>
 			</div>
 		</div>
 
@@ -58,8 +61,13 @@ export default {
 		}
 	},
 	methods: {
+		toggleImageLoading: function () {
+			document.getElementById('preview-img').classList.toggle('loading')
+		},
+
 		toggleOriginal: function () {
 			this.showOriginal = !this.showOriginal
+			this.toggleImageLoading()
 		},
 
 		getMedia: function (tokenMint) {
@@ -80,9 +88,32 @@ export default {
 <style scoped>
 .img-fluid {
 	max-width: 500px;
+	min-height: 500px;
+	min-width: 500px;
+	background: rgba(255, 255, 255, 0.6);
+	transition: all 0.3s ease;
+	border-radius: 7px;
+}
+
+.img-fluid.loading {
+	opacity: 0;
+	animation: fadeBg 3s ease-in-out infinite;
+}
+
+@keyframes fadeBg {
+	0% {
+		opacity: 0.4;
+	}
+	50% {
+		opacity: 1;
+	}
+	100% {
+		opacity: 0.4;
+	}
 }
 
 p {
 	color: white;
 }
+
 </style>
